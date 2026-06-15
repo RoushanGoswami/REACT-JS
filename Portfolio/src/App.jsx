47e09bf8f60953d0f0c63e61f98180791aa1391b
RoushanGoswami <roushangoswami1378@gmail.com>
2026-06-15 13:56:51 +0530

    ..

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -12,20 +12,20 @@ export default function App() {
   return (
     <>
     <nav>
-<Link to = {"/"}>Home</Link>
+<Link to = {"/login"}>Login</Link>
+<Link to = {"/home"}>Home</Link>
 <Link to = {"/about"}>About</Link>
-<Link to = {"/education"}>Education</Link>
 <Link to = {"/experience"}>Experience</Link>
 <Link to = {"/skills"}>Skills</Link>
 <Link to = {"/contact"}>Contact</Link>
     </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/experience" element={<Experience />} />
-        <Route path="/education" element={<Education />} />
       </Routes>
     </>
   );
--- Portfolio/src/screens/About.jsx
+++ Portfolio/src/screens/About.jsx
@@ -40,7 +40,6 @@ export default function About() {
           </div>
         </div>
       </section>
-      {/* hello */}
     </>
   );
 }
