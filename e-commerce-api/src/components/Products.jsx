25de52cb2ea9046ed39c669bb185c8972412c04b
RoushanGoswami <roushangoswami1378@gmail.com>
2026-06-24 17:41:07 +0530

    Update Products component

--- e-commerce-api/src/components/Products.jsx
+++ e-commerce-api/src/components/Products.jsx
@@ -3,21 +3,40 @@ import axios from "axios";
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
+  const [store, setStore] = useState([]);
   const handleFetchProducts = async () => {
     const res = await axios.get("https://dummyjson.com/products");
     setProducts(res.data.products);
+    setStore(res.data.products);
   };
 
   const getSearch = (e) => {
     setSearch(e.target.value);
   };
-  const handleSearch = () => {
+  const handleSearch = (e) => {
     setProducts(
-      products.filter(
+      store.filter(
         (product, i) =>
           product.title.toLowerCase().includes(search.toLowerCase()) ||
           product.category.toLowerCase().includes(search.toLowerCase()),
       ),
+    );
+  };
+  const handleCategoryFilter = (e) => {
+    setProducts(store);
+    if (e.target.value === "all") {
+      setProducts(store);
+    } else {
+      setProducts(
+        store.filter((product) => product.category == e.target.value),
+      ); //if selected product category matched to the e.target.value then it will show the filtered products using store (state)
+    }
+  };
+
+  const handlePriceRangeFilter = (e) => {
+    console.log(e.target.value);
+    setProducts(
+      store.filter((product) => product.price >= Number(e.target.value)), // product price is compared and then if condition is true the it returns
     );
   };
   useEffect(() => {
@@ -44,13 +63,24 @@ export default function Products() {
               >
                 Search
               </button>
-              <button
-                onClick={handleFetchProducts}
-                className="btn btn-outline-success"
-                type="submit"
+              <select
+                onChange={handleCategoryFilter}
+                className="text-black bg-light border border-success"
+                name=""
+                id=""
               >
-                Clear Filter
-              </button>
+                <option value="all">All</option>
+                <option value="beauty">Beauty</option>
+                <option value="fragrances">Fragrances</option>
+                <option value="furniture">Furniture</option>
+                <option value="groceries">Groceries</option>
+              </select>
+              <input
+                onChange={handlePriceRangeFilter}
+                type="range"
+                min={0}
+                max={2499.99}
+              />
             </div>
           </div>
         </div>
