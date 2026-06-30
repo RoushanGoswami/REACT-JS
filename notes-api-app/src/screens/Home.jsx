1355b55921ec20d1c436ebb04e9c718d4d5f8b21
RoushanGoswami <roushangoswami1378@gmail.com>
2026-06-30 15:03:49 +0530

    Update React Project

--- notes-api-app/src/screens/Home.jsx
+++ notes-api-app/src/screens/Home.jsx
@@ -4,31 +4,81 @@ import axios from "axios";
 export default function home() {
   // useState Declarations
   const [notes, setNotes] = useState([]); // ([]) to store it directly in the array
-
+  const [data, setData] = useState({});
   //get api of Notes
   const handleFetchNotes = async () => {
     const res = await axios.get("http://localhost:3000/notes");
     setNotes(res.data);
   };
 
+  // get input notes
+  const getNotesTitle = (e) => {
+    setData({ ...data, title: e.target.value });
+  };
+  const getNotesText = (e) => {
+    setData({ ...data, text: e.target.value });
+  };
+
+  //handleAddNote
+  const handleAddNote = async () => {
+    const res = await axios.post("http://localhost:3000/notes", data);
+  };
+  // delete notes
+  const handleDeleteNotes = async (id) => {
+    const res = await axios.delete("http://localhost:3000/notes/" + id);
+    handleFetchNotes(); // after delete fresh data will be showed
+  };
+  //update notes
+  const updateField = (x) => setData(x); // for just to onclick on edit btn it displays the current data
+  //in the input fields
+  // update function
+  const handleUpdateNotes = async () => {
+    const res = await axios.put("http://localhost:3000/notes/" + data.id, data);
+    handleFetchNotes();
+  };
+  useEffect(() => {
+    handleFetchNotes();
+  }, []);
   return (
     <>
       <section className="container">
         <h1 className="text-bg-primary text-dark p-3">Note App</h1>
         <div>
+          <div className="d-flex justify-content-start">
+            <input
+              onChange={getNotesTitle}
+              className="form-control text-dark bg-body mx-3 w-50"
+              type="text"
+              placeholder="Enter Title "
+              value={data.title}
+            />
+          </div>
           <div className="form-floating my-3 d-flex m-3 gap-3">
             <textarea
+              onChange={getNotesText}
+              value={data.text}
               className="form-control"
               placeholder="Write a note here..."
               id="floatingTextarea2"
               style={{ height: 50, width: 850 }}
-              defaultValue={""}
             />
             <label htmlFor="floatingTextarea2">Add a Note </label>
-            <button type="button" className="btn btn-outline-primary w-25">
+            <button
+              onClick={() => {
+                handleAddNote();
+                handleFetchNotes();
+                setData({ text: "", title: "" }); // make input Field clear after adding notes
+              }}
+              type="button"
+              className="btn btn-outline-primary w-25"
+            >
               Add Note
             </button>
-            <button type="button" className="btn btn-outline-primary w-25">
+            <button
+              onClick={handleUpdateNotes}
+              type="button"
+              className="btn btn-outline-primary w-25"
+            >
               Update Note
             </button>
           </div>
@@ -37,22 +87,31 @@ export default function home() {
 
       <section className="container">
         <h2 className="text-secondary my-3">Your Added Notes ...</h2>
-        <div className="row d-flex justify-content-center gap-3 ">
-          <div className="card" style={{ width: "18rem" }}>
-            <div className="card-body">
-              <h5 className="card-title">
-                Card title{" "}
-                <i className="ri-edit-box-fill text-warning fs-4 "></i>
-                <i class="ri-delete-bin-fill text-danger fs-4 "></i>
-              </h5>
+        {notes.length == 0 ? (
+          <p className="text-secondary">No Notes Added Yet !</p>
+        ) : (
+          notes.map((note, i) => (
+            <div key={i} className="row d-flex justify-content-center gap-3 ">
+              <div className="card" style={{ width: "18rem" }}>
+                <div className="card-body">
+                  <h5 className="card-title">
+                    {note.title}{" "}
+                    <i
+                      onClick={() => updateField(note)}
+                      className="ri-edit-box-fill text-warning fs-4 "
+                    ></i>
+                    <i
+                      onClick={() => handleDeleteNotes(note.id)}
+                      className="ri-delete-bin-fill text-danger fs-4 "
+                    ></i>
+                  </h5>
 
-              <p className="card-text text-start">
-                Some quick example text to build on the card title and make up
-                the bulk of the card’s content.
-              </p>
+                  <p className="card-text text-start">{note.text}</p>
+                </div>
+              </div>{" "}
             </div>
-          </div>{" "}
-        </div>
+          ))
+        )}
       </section>
     </>
   );
